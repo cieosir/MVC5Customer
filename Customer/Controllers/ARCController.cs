74eca5bf70674afa24e88bfa10ff964fb1a214f0
lieo <leelongkuang@gmail.com>
2018-07-13 23:42:45 +0800

    練習移除 WebFormViewEngine 以加速 View 頁面被找到的時間

---
+++ Customer/Controllers/ARCController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Customer.Controllers
+{
+    public class ARCController : Controller
+    {
+        // GET: ARC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewTest()
+        {
+            string model = "My Data";
+            return View((object)model);
+        }
+    }
+}
--- Customer/Global.asax.cs
+++ Customer/Global.asax.cs
@@ -16,6 +16,8 @@ namespace Customer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
