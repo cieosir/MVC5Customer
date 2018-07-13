c28e20dab9e88f7eba2d0909dcefc40b5f2565d7
lieo <leelongkuang@gmail.com>
2018-07-13 23:54:20 +0800

    22 練習用 FileResult 輸出一張圖片，並透過 ?dl=1 下載圖片

--- Customer/Controllers/ARCController.cs
+++ Customer/Controllers/ARCController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,6 +18,31 @@ namespace Customer.Controllers
         {
             string model = "My Data";
             return View((object)model);
+        }
+        public ActionResult PartialViewTest()
+        {
+            string model = "My Data";
+            return PartialView("ViewTest", (object)model);
+        }
+        public ActionResult ContentTest()
+        {
+            return Content("Test Content!",
+                "text/plain",
+                Encoding.GetEncoding("Big5"));
+        }
+
+        public ActionResult FileTest(string dl)
+        {
+            if (String.IsNullOrEmpty(dl))
+            {
+                return File(Server.MapPath("~/App_Data/fifa-18-world-cup.jpg"),
+                    "image/jpeg");
+            }
+            else
+            {
+                return File(Server.MapPath("~/App_Data/fifa-18-world-cup.jpg"),
+                    "image/jpeg", "My-FIFA-Picture.jpg");
+            }
         }
     }
 }
